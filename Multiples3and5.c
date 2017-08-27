24b4f9258db419ee47075f538ea48484dc4ce4e1
jonaylor89 <jonaylor89@gmail.com>
2017-08-27 14:42:49 -0400

    Initial Commit

---
+++ Multiples3and5.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(){
+
+  long sum = 0;
+
+  for(int i = 0; i < 1000; i++){
+    if((i % 3 == 0) || (i % 5 == 0)){
+      sum += i;
+    }
+  }
+
+  printf("%d\n", sum);
+
+  return 0;
+}
