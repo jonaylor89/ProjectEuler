b22817ead67be8b543c3161a3bfa66afbe467575
jonaylor89 <jonaylor89@gmail.com>
2017-08-27 23:05:14 -0400

    Initial Commit

---
+++ SumOfPrimes.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+  int isPrime;
+  long sum = 5;
+
+  for(int i = 5; i < 20000; i += 2){
+
+    isPrime = 1;
+
+    for(int j = 3; j < round(sqrt(i))+1; j += 2){
+      if(i % j == 0){
+        isPrime = 0;
+        break;
+      }
+    }
+
+    if(isPrime == 1){
+      sum += i;
+    }
+
+
+  }
+
+  printf("%d\n", sum);
+
+  return 0;
+}
