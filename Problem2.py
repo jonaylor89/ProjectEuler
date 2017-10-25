61b3d0bc76b7d87f0436864371104134b398c24a
John Naylor <jonaylor89@gmail.com>
2017-10-25 11:16:35 -0400

    Optimizing. Turning the list comprehension into a generator expression

--- Problem2.py
+++ Problem2.py
@@ -6,7 +6,7 @@ def fib(limit):
         a, b = a+b, a
         yield a
 
-        
+
 if __name__ == '__main__':
-    even_list = [x for x in fib(4000000) if x%2 == 0]
+    even_list = (x for x in fib(4000000) if x%2 == 0)
     print(sum(even_list))
