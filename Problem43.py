1d33c57bb93cb56b73d16b3c1b7ad361b1fe0135
John Naylor <jonaylor89@gmail.com>
2017-11-27 13:48:06 -0500

    Bug Fix

--- Problem43.py
+++ Problem43.py
@@ -15,26 +15,33 @@ def pandigital_generator():
                                 for h in np.delete(the_range, [a, b, c, d, e, f, g]):
                                     for i in np.delete(the_range, [a, b, c, d, e, f, g, h]):
                                         for j in np.delete(the_range, [a, b, c, d, e, f, g, h, i]):
-                                            yield str(a) + str(b) + str(c) + str(d) + str(e) + str(f) + str(g) + str(h) + str(i)
+                                            yield str(a) + str(b) + str(c) + str(d) + str(e) + str(f) + str(g) + str(h) + str(i) + str(j)
 
 
 def main():
 
     somme = 0
+    counter = 0
 
     for pandigit in pandigital_generator():
         num_string = pandigit
 
-        premier = int(num_string[1:5]) % 2 == 0
-        deuxieme = int(num_string[2:6]) % 3 == 0
-        troisieme = int(num_string[3:7]) % 5 == 0
-        quatrieme = int(num_string[4:8]) % 7 == 0
-        cinquieme = int(num_string[5:9]) % 11 == 0
-        sixieme = int(num_string[6:10]) % 13 == 0
-        septieme = int(num_string[7:11]) % 17 == 0
+        premier = int(num_string[1:4]) % 2 == 0
+        deuxieme = int(num_string[2:5]) % 3 == 0
+        troisieme = int(num_string[3:6]) % 5 == 0
+        quatrieme = int(num_string[4:7]) % 7 == 0
+        cinquieme = int(num_string[5:8]) % 11 == 0
+        sixieme = int(num_string[6:9]) % 13 == 0
+        septieme = int(num_string[7:10]) % 17 == 0
 
         if all([premier, deuxieme, troisieme, quatrieme, cinquieme, sixieme, septieme]):
-            somme += pandigit
+            somme += int(pandigit)
+
+        if counter % 100000 == 0:
+            print(counter)
+            print("\t", somme)
+
+        counter += 1
 
     print(somme)
 
