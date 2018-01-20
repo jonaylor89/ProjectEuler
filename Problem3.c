546a24cc9fecefa5117e29b23d9f963fb92f7bd4
John Naylor <jonaylor89@gmail.com>
2018-01-20 16:29:11 -0500

    Bug fixes

--- Problem3.c
+++ Problem3.c
@@ -5,11 +5,33 @@
 #define NUMBER 600851475143
 
 int isPrime(int test) {
-    int i = 0;
-    int calculateTo = ( (int) sqrt(test) );
+    int calculateTo = ( (int) floor(sqrt(test)) );
 
-    for(i = 3; i <= calculateTo; i += 2) {
+    if(test == 1) {
+        return 0; 
+    }
+
+    if(test < 4) {
+        return 1; 
+    }
+
+    if(test % 2 == 0) {
+        return 0; 
+    }
+
+    if(test < 9) {
+        return 1; 
+    }
+
+    if(test % 3 == 0) {
+        return 0; 
+    }
+
+    for(int i = 5; i <= calculateTo; i += 2) {
         if(test % i == 0) {
+            return 0; 
+        }
+        if(test % (i + 2) == 0) {
             return 0; 
         }
     }
@@ -19,9 +41,8 @@ int isPrime(int test) {
 
 int main() {
     int highest = 0;
-    int i = 0;
 
-    for(i = 3; i <= sqrt(NUMBER); i += 6) {
+    for(int i = 3; i <= floor(sqrt(NUMBER)); i += 2) {
         if(isPrime(i)) {
             if(NUMBER % i == 0)
                 highest = i;
--- Problem3.rs
+++ Problem3.rs
@@ -1,27 +1,57 @@
 
-fn isPrime(test: u64) -> bool {
-    let calculateTo = (test as f64).sqrt() as u64;
+fn is_prime(test: u64) -> bool {
+    let calculate_to = (test as f64).sqrt().floor() as u64;
 
-    for x in (3..calculateTo).step_by(6) {
+    if test == 1 {
+        return false; 
+    }
+
+    if test < 4 {
+        return true; 
+    }
+
+    if test % 2 == 0 {
+        return false; 
+    }
+
+    if test < 9 {
+        return true; 
+    }
+
+    if test % 3 == 0 {
+        return false; 
+    }
+
+    let mut x = 5;
+
+    while x < calculate_to {
         if test % x == 0 {
             return false;
         } 
+        if test % (x + 2) == 0 {
+            return false; 
+        }
+
+        x = x + 6;
     }
 
     return true;
 }
 
 fn main() {
-    let NUMBER: u64 = 600851475143;
-    let END: i32 = (NUMBER as f64).sqrt() as i32;
+    let number: u64 = 600851475143;
+    let end: i32 = (number as f64).sqrt() as i32;
     let mut highest = 0;
+    let mut i = 3;
 
-    for i in (3..END).step_by(6) {
-        if isPrime(i as u64) {
-            if NUMBER % i as u64 == 0 {
+    while i < end{
+        if is_prime(i as u64) {
+            if number % i as u64 == 0 {
                 highest = i; 
             } 
         }
+
+        i = i + 2;
     }
 
     println!("Highest prime factor {}", highest);
